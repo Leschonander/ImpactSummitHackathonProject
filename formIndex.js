5f30f40e826a5485565a6e877d67d2b868598aba
Leschonander <33600597+Leschonander@users.noreply.github.com>
2018-06-02 15:51:56 -0400

    Some more UI
    UI

--- formIndex.js
+++ formIndex.js
@@ -1,5 +1,8 @@
 Vue.component('vue-container', {
     template: '<div class="ui container raised segment vue-container">\
+                <div class="ui icon button" data-content="Pool Member"><i class="fas fa-users"></i></i></div>\
+                <div class="ui icon button" data-content="Institutional Member of Pool"><i class="fas fa-hospital"></i></div>\
+                <div class="ui icon button" data-content="Doctoral Member of Pool"><i class="fas fa-user-md"></i></div>\
                 <div class="ui icon button" data-content="User"><i class="user icon"></i></i></div>\
                 <slot></slot>\
                 <div class="ui labeled button vue-out" tabindex="0"><a class="ui basic label"> Log Out </a></div>\
@@ -8,11 +11,10 @@ Vue.component('vue-container', {
 
 });
 
-Vue.component('claim-feed', {
-    template: '<div class="ui message claimFeed"> \
-        <div class="header"> <slot name="head"> </slot> </div>\
-        <p> <slot name="info"> </slot> </p> </div>' 
-
+Vue.component('vue-form', {
+    template: '<form class="ui form"> \
+    <div class="field"> <label>Date</label> <input type="date" name="" placeholder="01/03/2009"> </div>\
+    </div>'
 });
 
 
